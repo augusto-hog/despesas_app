cd5352906986f6d2984939926ecc81f8714b129b
UEGAugusto <augusto@aluno.ueg.br>
2025-04-02 22:07:38 -0300

    refatora método de atualização de saldo: altera assinatura para aceitar transações antigas e novas, ajusta lógica de cálculo de saldo

--- lib/repositories/transaction_repository.dart
+++ lib/repositories/transaction_repository.dart
@@ -25,5 +25,8 @@ abstract class TransactionRepository {
 
   Future<DataResult<BalancesModel>> getBalances();
 
-  Future<void> updateBalance(TransactionModel newtTansaction);
+  Future<DataResult<BalancesModel>> updateBalance({
+    TransactionModel? oldTransaction,
+    required TransactionModel newTransaction,
+  });
 }

c9fc29112fcfa34598ddb8eb647e340581033235
UEGAugusto <augusto@aluno.ueg.br>
2025-04-02 22:31:23 -0300

    adiciona controle de perfil: implementa ProfileController e estados para gerenciar a recuperação de dados do usuário e suas respectivas respostas

--- lib/services/user_data_service/user_data_service.dart
+++ lib/services/user_data_service/user_data_service.dart
@@ -2,5 +2,5 @@ import 'package:despesas_app/common/data/data_result.dart';
 import 'package:despesas_app/common/models/models.dart';
 
 abstract class UserDataService {
-  DataResult<UserModel> getUserData();
+  Future<DataResult<UserModel>> getUserData();
 }
